09778e14ae08e6e114967aeceb3b053c640cbe1c
aramrw <106574385+aramrw@users.noreply.github.com>
2024-04-16 12:15:06 -0700

    fix forvo audio not serving

--- src/database.rs
+++ src/database.rs
@@ -14,22 +14,39 @@ pub struct Entry {
 pub async fn query_database(term: &str, reading: &str) -> Result<Vec<Entry>, Error> {
     let sqlite_pool = SqlitePool::connect("./audio/entries.db").await?;
 
-    let mut result = sqlx::query("SELECT * FROM entries WHERE expression = ? AND reading = ?")
+    let result = sqlx::query("SELECT * FROM entries WHERE expression = ? AND reading = ?")
         .bind(term)
         .bind(reading)
         .fetch_all(&sqlite_pool)
         .await?;
 
-    if result.is_empty() {
-        result = sqlx::query("SELECT * FROM entries WHERE expression = ?")
+    let forvo_result =
+        sqlx::query("SELECT * FROM entries WHERE expression = ? AND source = 'forvo'")
             .bind(term)
             .fetch_all(&sqlite_pool)
-            .await?; 
-    }
+            .await?;
 
     let mut query_entries: Vec<Entry> = Vec::new();
 
     result.iter().for_each(|row| {
+        let reading: Option<String> = row.try_get("reading").unwrap_or_default();
+        let speaker: Option<String> = row.try_get("speaker").unwrap_or_default();
+
+        // file starts with the folder name so cut it out
+        let mut file: String = row.get("file");
+        file = file.rsplit_once('\\').unwrap().1.to_string();
+
+        query_entries.push(Entry {
+            expression: row.try_get("expression").unwrap_or_default(),
+            reading,
+            source: row.get("source"),
+            speaker,
+            display: row.get("display"),
+            file,
+        });
+    });
+
+    forvo_result.iter().for_each(|row| {
         let reading: Option<String> = row.try_get("reading").unwrap_or_default();
         let speaker: Option<String> = row.try_get("speaker").unwrap_or_default();
 
