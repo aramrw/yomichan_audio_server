0c975a32673abcc9d575c7b190b2632a243fe242
aramrw <106574385+aramrw@users.noreply.github.com>
2024-05-26 19:58:52 -0700

    create entry struct

--- src/bin/json.rs
+++ src/bin/json.rs
@@ -4,3 +4,13 @@ use std::collections::hash_map::HashMap;
 use std::fs::File;
 use std::io::BufReader;
 
+#[derive(Default, Deserialize, Serialize, Debug)]
+pub struct Entry {
+    pub expression: String,
+    pub reading: Option<String>,
+    pub source: String,
+    pub speaker: Option<String>,
+    pub display: String,
+    pub file: String,
+}
+
