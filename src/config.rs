8fb8bd8be04d38a2a2661c7ddd90c1140801786e
aramrw <106574385+aramrw@users.noreply.github.com>
2024-12-30 19:46:17 -0800

    -- remove config

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
 #[cfg(target_os = "windows")]
 use std::os::windows::process::CommandExt;
-use std::{env::current_dir, path::Path};
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct Config {
@@ -9,33 +8,11 @@ pub struct Config {
     pub debug: bool,
 }
 
-pub fn create_config() -> Config {
-    let config_path = "config.json";
-    if !Path::new(config_path).exists() {
-        let config = Config {
-            exit_minutes: 30,
-            debug: false,
-        };
-
-        let config_json = serde_json::to_string_pretty(&config).unwrap();
-        std::fs::write(config_path, config_json).unwrap();
-    }
-
-    get_config()
-}
-
-fn get_config() -> Config {
-    let config_path = "config.json";
-    let config_json = std::fs::read_to_string(config_path).unwrap();
-    let config: Config = serde_json::from_str(&config_json).unwrap();
-
-    config
-}
-
 pub fn handle_debugger(hidden: bool) {
     if hidden {
         // Launch hidden instance
         #[cfg(target_os = "windows")]
+        #[allow(clippy::zombie_processes)]
         std::process::Command::new("yomichan_audio_server.exe")
             .arg("hidden")
             .creation_flags(0x00000008) // CREATE_NO_WINDOW
@@ -54,6 +31,7 @@ pub fn handle_debugger(hidden: bool) {
     if !hidden {
         // Launch visible instance
         #[cfg(target_os = "windows")]
+        #[allow(clippy::zombie_processes)]
         std::process::Command::new("yomichan_audio_server.exe")
             .arg("debug")
             .spawn()
