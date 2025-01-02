2d3a520d64ea89f735e56212d04e6840df690fe3
aramrw <106574385+aramrw@users.noreply.github.com>
2025-01-01 21:23:43 -0800

    Update helper.rs

--- src/helper.rs
+++ src/helper.rs
@@ -1,45 +1,91 @@
-use crate::database;
-//use rayon::prelude::*;
+use crate::database::{self, Entry};
 use bimap::BiHashMap;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use serde::{Deserialize, Serialize};
 use std::path::Path;
 use std::sync::LazyLock;
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct AudioSource {
-    name: String,
-    url: String,
+#[derive(Debug, thiserror::Error)]
+pub enum AudioFileError {
+    #[error("could not recognize audio source: {src}")]
+    UnkownSource { src: String },
+    #[error("unknown forvo speaker: {speaker}")]
+    UnknownForvoSpeaker { speaker: String },
+    #[error("missing entry speaker. forvo audio must contain a speaker: {forvo_speakers}")]
+    MissingForvoEntrySpeaker { forvo_speakers: String },
+    #[error("missing entry speaker: {file_name}")]
+    MissingAudioFile { file_name: String },
+    #[error("io error")]
+    Io(#[from] std::io::Error),
 }
 
-pub fn find_audio_file(entry: &database::Entry) -> Option<AudioSource> {
-    if entry.source == "shinmeikai8" {
-        let shinmeikai_dir_path = "audio/shinmeikai8_files/media";
-        return search_dir_helper("smk", &entry.display, shinmeikai_dir_path, &entry.file).ok();
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+pub struct AudioSource {
+    pub name: String,
+    pub url: String,
+}
+
+impl AudioSource {
+    pub fn create_list(entries: &[Entry]) -> Vec<AudioSource> {
+        // contruct the list of audio sources
+        let mut audio_sources_list: Vec<AudioSource> = Vec::with_capacity(entries.len());
+
+        if !entries.is_empty() {
+            let audio_files_res: Vec<AudioSource> = entries
+                .par_iter()
+                .filter_map(|e| Some(find_audio_file(e).unwrap())) // remove `None` values
+                .collect();
+
+            audio_sources_list = audio_files_res;
+        }
+        audio_sources_list
     }
 
-    if entry.source == "nhk16" {
-        let nhk16_dir_path = "audio/nhk16_files/media";
-        return search_dir_helper("nhk", &entry.display, nhk16_dir_path, &entry.file).ok();
+    pub fn print_list(list: &[AudioSource]) {
+        for entry in list {
+            println!("    ▼ {}\n        {}", entry.name, entry.url);
+        }
     }
+}
 
-    if entry.source == "daijisen" {
-        let daijisen_dir_path = "audio/daijisen_files/audio";
-        return search_dir_helper("daijisen", &entry.display, daijisen_dir_path, &entry.file).ok();
-    }
+pub fn find_audio_file(entry: &database::Entry) -> Result<AudioSource, AudioFileError> {
+    let jp_forvo_speakers = ["strawberrybrown", "kaoring", "poyotan", "akitomo", "skent"];
 
-    if entry.source == "jpod" {
-        let jpod_dir_path = "audio/jpod_files/audio";
-        return search_dir_helper("jpod", "", jpod_dir_path, &entry.file).ok();
-    }
+    match entry.source.as_str() {
+        "shinmeikai8" => {
+            let shinmeikai_dir_path = "audio/shinmeikai8_files/media";
+            search_dir_helper("smk", &entry.display, shinmeikai_dir_path, &entry.file)
+        }
+        "nhk16" => {
+            let nhk16_dir_path = "audio/nhk16_files/media";
+            search_dir_helper("nhk", &entry.display, nhk16_dir_path, &entry.file)
+        }
+        "daijisen" => {
+            let daijisen_dir_path = "audio/daijisen_files/media";
+            search_dir_helper("daijisen", &entry.display, daijisen_dir_path, &entry.file)
+        }
+        "jpod" => {
+            let jpod_dir_path = "audio/jpod_files/media";
+            search_dir_helper("jpod", "", jpod_dir_path, &entry.file)
+        }
+        // should be renamed to forvo_jp
+        "forvo" => {
+            let Some(entry_speaker) = entry.speaker.clone() else {
+                return Err(AudioFileError::MissingForvoEntrySpeaker {
+                    forvo_speakers: jp_forvo_speakers.join(" "),
+                });
+            };
 
-    if entry.source == "forvo" {
-        let forvo_speakers = ["strawberrybrown", "kaoring", "poyotan", "akitomo", "skent"];
-        for speaker in forvo_speakers.iter() {
-            if speaker != entry.speaker.as_ref().unwrap() {
-                continue;
-            }
+            let Some(current_speaker) = jp_forvo_speakers
+                .into_iter()
+                .find(|speaker| *speaker == entry_speaker)
+            else {
+                return Err(AudioFileError::UnknownForvoSpeaker {
+                    speaker: entry_speaker,
+                });
+            };
 
-            let format_dir = format!("audio/forvo_files/{}", speaker);
+            let format_dir = format!("audio/jp_forvo_files/{}", current_speaker);
 
             search_dir_helper(
                 entry.speaker.as_ref().unwrap(),
@@ -47,21 +93,18 @@ pub fn find_audio_file(entry: &database::Entry) -> Option<AudioSource> {
                 &format_dir,
                 &entry.file,
             )
-            .ok();
-            //println!("returning {:?}", audio_source);
         }
+        "forvo_zh" => search_dir_helper_forvo(
+            entry.speaker.as_deref().unwrap(),
+            "",
+            "audio/zh",
+            &entry.file,
+        ),
+
+        _ => Err(AudioFileError::UnkownSource {
+            src: entry.source.clone(),
+        }),
     }
-
-    if entry.source == "forvo_zh" {
-        let audio_source =
-            search_dir_helper_forvo(entry.speaker.as_ref().unwrap(), "", "audio/zh", &entry.file)
-                .unwrap();
-
-        println!("returning {:?}", audio_source);
-        return audio_source;
-    }
-
-    None
 }
 
 fn search_dir_helper_forvo(
@@ -69,52 +112,44 @@ fn search_dir_helper_forvo(
     entry_display: &str,
     main_dir: &str,
     file_name: &str,
-) -> Result<Option<AudioSource>, std::io::Error> {
+) -> Result<AudioSource, AudioFileError> {
     // Iterate over each folder in the main directory
     for folder in std::fs::read_dir(main_dir)? {
         let folder = folder?;
         let folder_name = folder.file_name();
-        let folder_name_str = folder_name.to_string_lossy(); // Convert to &str for comparison
+        let folder_name_str = folder_name.to_string_lossy();
         let folder_path = folder.path();
 
         // Skip folders that do not match the speaker name
         if folder_name_str != speaker {
-            continue; // Continue to the next folder if names do not match
+            continue;
         }
 
         // Search for the file within the matched folder
-        let result = std::fs::read_dir(&folder_path)?.find_map(|entry| {
-            match entry {
-                Ok(entry) => {
-                    let entry_file_name = entry.file_name();
-                    if entry_file_name == file_name {
-                        // Construct and return the audio source if file matches
-                        Some(construct_audio_source(
-                            speaker,
-                            entry_display,
-                            &format!("{}/{}", main_dir, speaker),
-                            file_name,
-                        ))
-                    } else {
-                        None
-                    }
-                }
-                Err(e) => {
-                    // Log any errors reading directory entries
-                    eprintln!("Error reading directory entry: {}", e);
-                    None
-                }
+        let final_audio = std::fs::read_dir(&folder_path)?.find_map(|entry| {
+            let entry = entry.ok()?;
+            let entry_file_name = entry.file_name();
+            if entry_file_name == file_name {
+                // Construct and return the audio source if file matches
+                let audio_src = construct_audio_source(
+                    speaker,
+                    entry_display,
+                    &format!("{}/{}", main_dir, speaker),
+                    file_name,
+                );
+                return Some(audio_src);
             }
+            None
         });
 
-        // Return the result if found
-        if result.is_some() {
-            return Ok(result);
+        if let Some(final_audio) = final_audio {
+            return Ok(final_audio);
         }
     }
 
-    // Return None if no file was found
-    Ok(None)
+    Err(AudioFileError::MissingAudioFile {
+        file_name: file_name.to_string(),
+    })
 }
 
 fn search_dir_helper(
@@ -122,9 +157,9 @@ fn search_dir_helper(
     entry_display: &str,
     main_dir: &str,
     file_name: &str,
-) -> Result<AudioSource, std::io::Error> {
+) -> Result<AudioSource, AudioFileError> {
     let file_path = Path::new(main_dir).join(file_name);
-    println!("searching path {:#?}", file_path);
+    //println!("searching path {:#?}", file_path);
     std::fs::File::open(file_path)?;
 
     Ok(construct_audio_source(
@@ -156,23 +191,6 @@ fn construct_audio_source(
         url: format!("http://localhost:8080/{}/{}", main_dir, file_name),
     }
 }
-
-// pub fn convert_kana(term: &str) -> String {
-//     let chars = term.chars();
-//
-//     chars
-//         .map(|c| {
-//             let mut tmp = [0u8];
-//             let str = c.encode_utf8(&mut tmp);
-//             match KANA_MAP.get_by_left(str) {
-//                 Some(hg) => *hg,
-//                 None => *KANA_MAP.get_by_right(str).unwrap(),
-//             }
-//         })
-//         .collect::<Vec<&str>>()
-//         .concat()
-// }
-
 #[rustfmt::skip]
 pub static KANA_MAP: LazyLock<BiHashMap<&'static str, &'static str>> = LazyLock::new(|| {
     BiHashMap::from_iter([
