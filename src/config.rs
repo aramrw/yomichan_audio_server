5a3a194f4659c4137312e5048d1782283d3685a1
aramrw <106574385+aramrw@users.noreply.github.com>
2025-03-20 22:48:07 -0700

    update config

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use std::env::current_dir;
+use std::env::{current_dir, current_exe};
 // needed for Command's 'creation_flags' method.
 #[cfg(target_os = "windows")]
 use std::os::windows::process::CommandExt;
@@ -15,10 +15,11 @@ pub struct Config {
 
 pub fn spawn_headless() {
     let audio_path = &PROGRAM_INFO.get().unwrap().cli.audio;
+    let binary_path = std::env::current_exe().unwrap();
     // Launch hidden instance
     #[cfg(target_os = "windows")]
     #[allow(clippy::zombie_processes)]
-    std::process::Command::new("yomichan_audio_server.exe")
+    std::process::Command::new(binary_path)
         .creation_flags(0x00000008) // CREATE_NO_WINDOW
         .args([
             "--audio",
@@ -31,7 +32,6 @@ pub fn spawn_headless() {
 
     #[cfg(target_os = "macos")]
     {
-        let binary_path = std::env::current_exe().unwrap();
         std::process::Command::new(binary_path)
             .args([
                 "--audio",
@@ -48,11 +48,14 @@ pub fn spawn_headless() {
 }
 
 pub fn kill_previous_instance() {
+    let current_exe = current_exe().unwrap();
     let mut sys = sysinfo::System::new();
     sys.refresh_processes();
 
     for (pid, proc) in sys.processes() {
-        if proc.name().contains("yomichan_audio_server") && pid.as_u32() != std::process::id() {
+        if proc.name() == current_exe.file_name().unwrap().to_string_lossy()
+            && pid.as_u32() != std::process::id()
+        {
             println!("Killing previous instance with PID: {}", pid);
             proc.kill();
         }
